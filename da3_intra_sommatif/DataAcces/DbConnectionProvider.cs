ee045641a4fb86cdf8a9c9bbb9ea49ffdac49056
AlejoB <alejoberrio0105@gmail.com>
2024-10-21 08:25:03 -0400

    Add project files.

---
+++ da3_intra_sommatif/DataAcces/DbConnectionProvider.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace da3_intra_sommatif.DataAcces
+{
+    internal class DbConnectionProvider
+    {
+        private static string connecionString = "Server= .\\SQL2022DEV;Integrated Security=true;Database=da3_intra_sommatif;TrustServerCertificate=true;";;
+        private static SqlConnection? connection;
+
+        public static SqlConnection GetConnection()
+        {
+            connection ??= new SqlConnection(connecionString);
+            return connection;
+        }
+    }
+}
